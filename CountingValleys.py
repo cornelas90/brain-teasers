1b64b0ba47df5baf2444b87691f25fea4ae3e42f
Christian Ornelas <81385494+cornelas90@users.noreply.github.com>
2021-05-05 12:40:55 -0500

    Counts mountains too! TM

--- CountingValleys.py
+++ CountingValleys.py
@@ -1,5 +1,13 @@
 '''
-A hike has some number of steps and a tracked path of ascents or descents. Each hike starts and ends at sea level.
+A hike has some number of steps and a tracked path of ascents or descents. Each hike starts and ends at sea level. This program counts the number of valleys and mountains.
+
+                   /\
+                  /  \
+     /\          /    \
+____/  \      /\/      \_____
+        \    /
+         \/\/
+
 
 Sample Input:
 20
@@ -8,12 +16,16 @@ UUDDDDUDUUUDUUUUDDDD
 '''
 
 if __name__ == '__main__':
-    steps = range(int(input()))
-    path = [1 if item =='U' else -1 for item in list(input())]      #  convert ascent and descent to numerical values
-    x = 0                                                           #  elevation
-    valley=0
+    steps = range(int(input('Number of Steps: ')))
+    path = [1 if item == 'U' else -1 for item in list(input('Elevation Path: '))]           #  convert ascent and descent to numerical values
+    x = 0                                                
+    valley = 0
+    mountain = 0
     for i in steps:
-        x+=path[i]
-        if x <0 and x + path[i+1] == 0:                             #  One valley for each elevation drop below sea level and rises back to sea level
-            valley+=1
-    print(valley)
+        x += path[i]                                #  elevation tracker 
+        if x < 0 and x + path[i+1] == 0:            #  One valley for elevation drop below sea level and rises back to sea level, vise versa for mtns
+            valley += 1
+        elif x > 0 and x + path[i+1] == 0:
+        	mountain += 1
+    print('Number of Valleys:', valley)
+    print('Number of Mountains:', mountain)
